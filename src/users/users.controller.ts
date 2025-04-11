8d5571a28dd9c0b477da97dad359dec046f6c49a
Gian Munoz <gian0291@gmail.com>
2025-04-10 22:00:56 -0500

    Se cargo la aplicación de PizzLab app

---
+++ src/users/users.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@Controller('users')
+@ApiTags('Users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  @ApiOperation({ summary: 'Crear nuevos usuarios' })
+  create(@Body() user: CreateUserDto) {
+    return this.usersService.create(user);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Mostrar todos los usuarios' })
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
