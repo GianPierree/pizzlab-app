b0589d9b66365f3c27b0b70dc9961fd7914fc04c
Gian Munoz <gian0291@gmail.com>
2025-04-17 00:34:49 -0500

    fix: :recycle: Se agrego nuevamente todos los archivos

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/pizzlab_db'),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'pizzlab_db',
+      autoLoadEntities: true,
+      synchronize: true, //TODO: Solo en desarrollo, en producción no se debe usar
+    }),
+    AuthModule,
+    ProductsModule
+  ],
+})
+export class AppModule {}
---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,18 @@
+import { 
+  IsNotEmpty, 
+  IsString, 
+  MinLength 
+} from 'class-validator';
+
+export class LoginDto {
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  password: string;
+}
